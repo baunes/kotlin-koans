0a7168c9aea34301a0a0dd089e821adf4dc54828
Javier Bauset <baunes@protonmail.com>
2019-10-08 21:09:17 +0200

    Conventions Koans

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,18 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator() = DateIterator(this)
+}
+
+class DateIterator(private val dateRange: DateRange) : Iterator<MyDate> {
+    private var current: MyDate = dateRange.start
+
+    override fun hasNext() = current <= dateRange.end
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -4,12 +4,16 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val times: Int)
+operator fun TimeInterval.times(times: Int) = RepeatedTimeInterval(this, times)
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate =
+        addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.times)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
